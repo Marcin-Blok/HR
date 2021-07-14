83bad4d7b3a65001034b34c7851d53f8ee4de7ca
Marcin-Blok <marcinblok7980@gmail.com>
2021-07-14 15:25:51 +0200

    #5 Utworzenie klasy Pracownik z polami, konstruktorem, metodami toString() oraz czyPracujeWDziale()

--- src/com/marcinblok/company/Main.java
+++ src/com/marcinblok/company/Main.java
@@ -3,6 +3,8 @@ package com.marcinblok.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int dzial = 1;
+	    Pracownik p = new Pracownik("Marcin" , "Blok", 7800, 'M', 1);
+        System.out.println("Czy pracownik pracuje w dziale " + dzial + "? " + p.czyPracujeWdziale(dzial));
     }
 }
